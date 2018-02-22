16ef9a2487151834886b6649473a3d28a7f7bffe
SturmkitX <bogdanius96@gmail.com>
2018-02-22 13:57:09 +0200

    initial commit bogdan (#14)
    * Did nothing. Please let me go
    
    * initial commit bogdan
    
    * Printed my name
    
    * restored original readme
    
    * initial commit bogdan
    
    * initial commit bogdan
    
    * restored original readme

--- main/main.go
+++ main/main.go
@@ -9,4 +9,5 @@ func main() {
 	fmt.Println("Hello my name is Alex")
 	fmt.Println("And I want to merge my changes")
 	fmt.Println("hello my name is alin")
+	fmt.Println("Hello my nombre es Bogdan")
 }
